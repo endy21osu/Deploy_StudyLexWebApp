407071eb6030549917c20706def1c5d5384c94ad
endy21osu <endy21osu@gmail.com>
2016-10-03 17:40:06 -0400

    Update server.js
    updating dev login

--- server.js
+++ server.js
@@ -8,7 +8,8 @@ var express = require('express'),
     LocalStrategy = require('passport-local').Strategy;
 
 // mongoose.connect('mongodb://13.85.68.235:27017/SkillsDB'); // Prod
-mongoose.connect('mongodb://40.84.158.230:27017/SkillsDB'); // Dev
+//mongoose.connect('mongodb://username:password@host:port/database?options...');
+mongoose.connect('mongodb://elev8incDevuser:ktdaSico2016@40.84.158.230:27017/SkillsDB'); // Dev
 // mongoose.connect('mongodb://localhost:27017/SkillsDB');
 
 app.use(bodyParser.json());
