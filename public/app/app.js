ed5bc13e908c8b25ef7f80bfd1aa3a7ba13c700d
jgordon <endy21osu@gmail.com>
2016-10-05 05:57:40 -0400

    this is the guide portion set up by chatura

--- public/app/app.js
+++ public/app/app.js
@@ -53,6 +53,10 @@ angular.module('myApp', [
               url: "/register",
               templateUrl: './app/components/register/register.html',
               controller: 'RegisterCtrl'
+            })
+            .state("guide",{
+              url:"/guide",
+              templateUrl:'./app/components/guide/guide.html'
             });
 }])
 
